8b8eff919703476f094be76969c31cbddfbcca8f
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-13 02:15:55 +0000

    Update salary.c

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -16,25 +16,22 @@
     float nonTaxable = 12500.00;
     float NIContribution;
     float salaryAfterNI;
+    float taxableAmount;
     float TaxContribution;
     float salaryAfterTax;
 
     // calculate the deductions and final take-home salary
     NIContribution = salary * NI;
     salaryAfterNI = salary - NIContribution;
-    salaryAfterNI = salaryAfterNI - nonTaxable;
-    TaxContribution = salaryAfterNI * tax;
+    taxableAmount = salaryAfterNI - nonTaxable;
+    TaxContribution = taxableAmount * tax;
     salaryAfterTax = salaryAfterNI - TaxContribution;
 
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    printf("Salary £...%.2f",salary);
-    //printf("NI contribution £...",var_name);
-    printf("NI contribution £...%.2f",NIContribution);
-    //printf("Tax contribution £...",var_name);
-    printf("Tax contribution £...%.2f",TaxContribution);
-    //printf("Take home salary £...",var_name);
-    printf("Take home salary £...%.2f",salaryAfterTax);
+    // required print statements
+    printf("Salary £...%.2f\n", salary);
+    printf("NI contribution £...%.2f\n", NIContribution);
+    printf("Tax contribution £...%.2f\n", TaxContribution);
+    printf("Take home salary £...%.2f\n", salaryAfterTax);
 
     return 0;
- }
+}
